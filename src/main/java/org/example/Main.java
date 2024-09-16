f557fe71c88870db43c327e13cad736d77508f4d
Ayberk <ayberkcetinkaya54@gmail.com>
2024-09-16 22:00:22 +0300

    commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,29 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println(shouldWakeUp(true, 1));
+        //System.out.println(shouldWakeUp(false, 2));
+        //System.out.println(shouldWakeUp(true, 8));
+        //System.out.println(shouldWakeUp(true, -1));
+        System.out.println(hasTeen(9,99,19));
+        System.out.println(hasTeen(23,15,42));
+        System.out.println(hasTeen(22,23,34));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23 || !isBarking) {
+            return false;
+        }
+        return clock >= 20 || clock < 8;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
-    }
-
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
-    }
-
-    public static double area(double width, double height) {
-       
-    }
-
-    public static double area(double radius) {
-       
+        if (firstAge >= 13 && firstAge<=19 || secondAge>=13 && secondAge<=19 || thirdAge>=13 && thirdAge<=19 ) {
+            return true;
+        }
+        return false;
     }
 }
+
+
+
